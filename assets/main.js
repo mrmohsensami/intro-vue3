197fe78c749b30ab5d8ac2219dfa015d15401fe3
MohsenSami <mohsensami88@gmail.com>
2022-08-05 17:38:18 +0430

    components and props

--- assets/main.js
+++ assets/main.js
@@ -2,50 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            product: 'Sockes',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            sizes: ['S', 'M', 'L', 'XL'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpeg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpeg', quantity: 0 },
-            ],
-            // solution
-            onSale: true
-            // solution
+            premium: true
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        // solution
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' is on sale.'
-            }
-            return ''
-        }
-        // solution
-    }
+    methods: {},
 })
