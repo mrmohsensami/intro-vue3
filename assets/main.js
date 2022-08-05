28dfd79e1eebd3b00ca4ee3bec7eed4a1081edc7
MohsenSami <mohsensami88@gmail.com>
2022-08-05 16:26:12 +0430

    list rendering

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,13 @@ const app = Vue.createApp({
         return {
             product: 'Sockes',
             image: './assets/images/socks_green.jpeg',
-            inventory: 0
+            inventory: 5,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+              { id: 2234, color: 'green' },
+              { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
