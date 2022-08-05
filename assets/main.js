09dc2595e877d753067581356a1a03bce18c5e69
MohsenSami <mohsensami88@gmail.com>
2022-08-05 16:36:42 +0430

    Event handling

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,29 @@
 const app = Vue.createApp({
-    data: function() {
+    data() {
         return {
+            cart: 0,
             product: 'Sockes',
             image: './assets/images/socks_green.jpeg',
             inventory: 5,
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-              { id: 2234, color: 'green' },
-              { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpeg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpeg' },
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
